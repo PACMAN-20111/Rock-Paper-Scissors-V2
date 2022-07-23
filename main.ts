e3231745e2594f03e1b9c1ab20ca35f3d17c9a9b
PACMAN-20111 <109835290+PACMAN-20111@users.noreply.github.com>
2022-07-23 09:57:23 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+let Hand = 0
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    music.setBuiltInSpeakerEnabled(true)
+})
+input.onButtonPressed(Button.A, function () {
+    led.setBrightness(50)
+})
+input.onButtonPressed(Button.AB, function () {
+    led.setBrightness(255)
+})
+input.onButtonPressed(Button.B, function () {
+    led.setBrightness(150)
+})
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.setBuiltInSpeakerEnabled(false)
+})
