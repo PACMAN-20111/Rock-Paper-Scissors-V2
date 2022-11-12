d472526a778fb942480e269420a0061d24806fef
PACMAN-20111 <109835290+PACMAN-20111@users.noreply.github.com>
2022-11-12 11:22:15 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
-let Hand = 0
+let _1 = 0
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     music.setBuiltInSpeakerEnabled(true)
 })
 input.onButtonPressed(Button.A, function () {
-    led.setBrightness(50)
+    led.setBrightness(100)
 })
 input.onButtonPressed(Button.AB, function () {
     led.setBrightness(255)
 })
 input.onButtonPressed(Button.B, function () {
-    led.setBrightness(150)
+    led.setBrightness(180)
 })
 input.onGesture(Gesture.Shake, function () {
-    Hand = randint(1, 3)
-    if (Hand == 1) {
+    _1 = randint(1, 3)
+    if (_1 == 1) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
         basic.showIcon(IconNames.Square)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    } else if (Hand == 2) {
+    } else if (_1 == 2) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
         basic.showIcon(IconNames.SmallSquare)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     } else {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
         basic.showIcon(IconNames.Scissors)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
